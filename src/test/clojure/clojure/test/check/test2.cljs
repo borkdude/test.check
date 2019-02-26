5464ea47c0e614c0d7066ba65c5a9b81d3caf1dc
Michiel Borkent <michielborkent@gmail.com>
2019-02-26 15:23:29 +0100

    WIP

--- src/test/clojure/clojure/test/check/test2.cljs
+++ src/test/clojure/clojure/test/check/test2.cljs
@@ -1,6 +1,9 @@
 (ns clojure.test.check.test2
-  (:require [cljs.test :refer-macros [deftest is]]
+  (:require [cljs.test :as t :refer-macros [deftest is]]
             [clojure.test.check.generators :as gen]))
 
 (deftest can-use-gen-let-without-include-macros
-  (is (gen/let [x gen/nat] 42)))
+  (is (gen/let [x gen/nat]
+        (number? x))))
+
+(t/run-tests)
